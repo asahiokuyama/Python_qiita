d8759936794927cf3bffcd4ff0fdbe1d02755840
Asahi Okuyama <59007694+asahiokuyama@users.noreply.github.com>
2022-01-05 17:13:07 +0900

    Add files via upload

---
+++ ex1.py
@@ -0,0 +1,18 @@
+#1
+print("自分のなまえ")
+
+#2
+print(60*60*24)
+print(1296000/86400)
+
+#3
+mylist = [0,1,2,3,4,5,6,7,8,9]
+print(mylist)
+del mylist[5]
+print(mylist)
+mylist.append(5)
+print(mylist)
+mylist.insert(0,95)
+print(mylist)
+mylist.sort()
+print(mylist)
